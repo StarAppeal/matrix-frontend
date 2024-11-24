7f9f057a21dd91e7c255abb1133ef7e6a57aa89b
StarAppeal <info@starappeal.app>
2024-11-24 15:39:08 +0100

    add spotify authentication as a button to the app

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,21 +1,37 @@
-import { Text, View, StyleSheet } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from "react";
+import SpotifyAuthButton from "@/components/SpotifyAuthButton";
+import {Token} from "@/services/RestService";
 
 export default function AboutScreen() {
-  return (
-      <View style={styles.container}>
-        <Text style={styles.text}>About screen</Text>
-      </View>
-  );
+    const [token, setToken] = useState<Token | null>(null);
+
+    const handleAuthSuccess = (token: Token) => {
+        setToken(token);
+        console.log('Erhaltener Authentifizierungscode:', token);
+        // Hier kannst du den Code weiterverwenden, um das Access Token zu erhalten
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>About screen</Text>
+            <Text>Willkommen bei der Spotify Authentifizierung</Text>
+            <SpotifyAuthButton onAuthSuccess={handleAuthSuccess}/>
+            {token && <Text>Erhaltener Code: {token.access_token}</Text>}
+        </View>
+
+
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#fff',
+    },
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,62 +1,65 @@
 import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
-
-const PlaceholderImage = require('@/assets/images/GarfieldCharakter.webp');
-
 import * as ImagePicker from 'expo-image-picker';
 import {RestService} from '@/services/RestService';
 import useService from '@/hooks/useService';
 
+const PlaceholderImage = require('@/assets/images/GarfieldCharakter.webp');
+
+import * as WebBrowser from "expo-web-browser";
+
+WebBrowser.maybeCompleteAuthSession();
+
 export default function Index() {
-  const {data, loading, error} = useService(RestService.fetchAllUser);
+    const {data: usersData, loading: userLoading, error: userError} = useService(RestService.fetchAllUser);
 
-  const pickImageAsync = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ['images'],
-      allowsEditing: true,
-      quality: 1,
-    });
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images'],
+            allowsEditing: true,
+            quality: 1,
+        });
 
-    if (!result.canceled) {
-      console.log(result);
-    } else {
-      alert('You did not select any image.');
-    }
-  };
+        if (!result.canceled) {
+            console.log(result);
+        } else {
+            alert('You did not select any image.');
+        }
+    };
 
-  console.log(data);
-  return (
-      <View style={styles.container}>
-        <View style={styles.imageContainer}>
-          <ImageViewer imgSource={PlaceholderImage}/>
+    console.log(usersData);
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer imgSource={PlaceholderImage}/>
+            </View>
+            <View style={styles.footerContainer}>
+                {userLoading && <ActivityIndicator/>}
+                {userError && <Text>Error: {userError.message}</Text>}
+                {usersData && (
+                    <Text>
+                        {usersData.users.map((item) => item.name).join('; ')}
+                    </Text>
+                )}
+                <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/>
+                <Button label="Use this photo"/>
+            </View>
         </View>
-        <View style={styles.footerContainer}>
-          {loading && <ActivityIndicator/>}
-          {error && <Text>Error: {error.message}</Text>}
-          {data && (
-              <Text>
-                {data.users.map((item) => item.name).join('; ')}
-              </Text>
-          )}
-          <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/>
-          <Button label="Use this photo"/>
-        </View>
-      </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-  },
-  imageContainer: {
-    flex: 1,
-  },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
 });
