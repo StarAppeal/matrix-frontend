0ab2fa2c329fce6b9973a4b85860c9fc9e025031
StarAppeal <info@starappeal.app>
2024-12-06 21:24:17 +0100

    add tabs (prototype)

--- src/components/themed/ThemedHeader.tsx
+++ src/components/themed/ThemedHeader.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import {StyleSheet} from "react-native";
 import {Text} from "react-native-paper";
-import {useTheme} from "@/src/context/ThemeProvider";
 
 type Props = {
     children: React.ReactNode;
 }
 
 export default function ThemedHeader(props: Props) {
-    const {theme} = useTheme();
     return <Text style={styles.header} {...props} />;
 }
 
