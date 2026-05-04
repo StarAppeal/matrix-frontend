0aec6e3bbc5b6f1fee1bead17504c6b20034200a
StarAppeal <info@starappeal.app>
2026-05-04 20:44:01 +0200

    chore: remove unused ScrollView import in game_of_life.tsx

--- app/(tabs)/modes/game_of_life.tsx
+++ app/(tabs)/modes/game_of_life.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text, ScrollView} from "react-native";
+import {View, Text} from "react-native";
 import {useMatrixStore} from "@/src/stores";
 import ColorSelector from "@/src/components/themed/ColorSelector";
 import ThemedSegmentedButtons from "@/src/components/themed/ThemedSegmentedButtons";
